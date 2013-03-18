c9d896ad3346d6dcdb908a64ae2580ca55e82c39
Matthew McMillan <matthew.mcmillan@gmail.com>
2013-03-18 12:02:47 -0500

    More comments

--- check-template-json-webservice.py
+++ check-template-json-webservice.py
@@ -6,7 +6,8 @@
 #
 #
 # Author: Matthew McMillan - matthew.mcmillan@gmail.com
-#
+# Blog: http://matthewcmcmillan.blogspot.com
+# Code repo: https://github.com/matt448/nagios-checks
 #
 # Nagios error codes
 # 0 = OK
